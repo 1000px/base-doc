c907b683e4fb033ceb57672e0fd2fda04e38f517
malixiang <malixiang@kedacom.com>
2018-05-16 13:26:13 +0800

    添加假数据，修改webpack配置加入scssloader等，添加markdown转html插件

--- src/view/guide/index.vue
+++ src/view/guide/index.vue
@@ -5,31 +5,62 @@
 -->
 <template>
 	<div class="guide">
-		<v-nav></v-nav>
+		<k-nav :leftList="guideList"></k-nav>
 		<div class="guide-content">
-			<h1>{{msg}}</h1>
+			<div v-html="contentMd"></div>
 		</div>
 	</div>
 </template>
 
 <script>
-import vNav from '@/components/nav';
+// 引入公用组件
+import kNav from '@/components/nav';
+
+// 引入数据服务
+import {getGuideList, getGuideItem} from '@/service/guide';
+
+// 引入其它服务
+import marked from 'marked';
+
 export default {
 	name: 'guide',
 	data () {
 		return {
-			msg: 'vue-base脚手架快速引导页'
+			msg: 'vue-base脚手架快速引导页',
+			guideList: [],
+			contentMd: ''
 		};
 	},
+	computed: {
+
+	},
+	mounted () {
+		getGuideList().then(res => {
+			this.guideList = res[10001];
+			console.log(10002, this.guideList);
+			// 获取当前第一个导航的内容
+			getGuideItem().then(res => {
+				console.log(10001, res);
+				this.contentMd = marked(res);
+			}).catch(err => {
+				console.log(err);
+			});
+		}).catch(err => {
+			console.log(err);
+		});
+	},
 	components: {
-		vNav
+		kNav
 	}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang='scss' scoped>
 h1 {
 	font-weight: normal;
+	span {
+		color: red;
+	}
 }
 </style>
