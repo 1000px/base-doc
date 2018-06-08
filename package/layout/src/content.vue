233a203b5ea160813d364fa7e6e45c4acb812c60
liky <liqi_jkcpx@kedacom.com>
2018-06-08 17:40:16 +0800

    '修改button样式'

---
+++ package/layout/src/content.vue
@@ -0,0 +1,16 @@
+<template>
+	<div :class="wrapClasses">
+		<slot></slot>
+	</div>
+</template>
+<script>
+	const prefixCls = 'ivu-layout';
+	export default {
+		name: 'KcContent',
+		computed: {
+			wrapClasses () {
+				return `${prefixCls}-content`;
+			}
+		}
+	};
+</script>
